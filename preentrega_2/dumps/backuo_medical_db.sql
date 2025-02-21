09190aa838c4607306786e8ebd6595f100c25970
fernando valdes <valdesmartin@gmail.com>
2025-02-21 13:36:41 -0300

    add diagram

--- preentrega_2/dumps/backuo_medical_db.sql
+++ preentrega_2/dumps/backuo_medical_db.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `medical_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `medical_system`;
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: localhost    Database: medical_system
 -- ------------------------------------------------------
--- Server version	8.0.40
+-- Server version	8.0.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,6 +30,7 @@ CREATE TABLE `consultation` (
   `doctor_id` int NOT NULL,
   `date` date NOT NULL,
   `notes` text,
+  `state` varchar(255) NOT NULL DEFAULT 'pendiente',
   PRIMARY KEY (`id`),
   KEY `patient_id` (`patient_id`),
   KEY `doctor_id` (`doctor_id`),
@@ -42,8 +45,36 @@ CREATE TABLE `consultation` (
 
 LOCK TABLES `consultation` WRITE;
 /*!40000 ALTER TABLE `consultation` DISABLE KEYS */;
-INSERT INTO `consultation` VALUES (1,1,1,'2025-01-01','evf');
+INSERT INTO `consultation` VALUES (1,1,1,'2000-01-01','Status updated: 1 (1) at 2025-02-21 12:58:45','1');
 /*!40000 ALTER TABLE `consultation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `consultation_audit`
+--
+
+DROP TABLE IF EXISTS `consultation_audit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `consultation_audit` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `consultation_id` int NOT NULL,
+  `previous_state` varchar(255) NOT NULL,
+  `change_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `consultation_id` (`consultation_id`),
+  CONSTRAINT `consultation_audit_ibfk_1` FOREIGN KEY (`consultation_id`) REFERENCES `consultation` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `consultation_audit`
+--
+
+LOCK TABLES `consultation_audit` WRITE;
+/*!40000 ALTER TABLE `consultation_audit` DISABLE KEYS */;
+INSERT INTO `consultation_audit` VALUES (1,1,'pendiente','2025-02-21 15:58:45');
+/*!40000 ALTER TABLE `consultation_audit` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -73,7 +104,7 @@ CREATE TABLE `doctor` (
 
 LOCK TABLES `doctor` WRITE;
 /*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
-INSERT INTO `doctor` VALUES (1,'Dr. John Smith',12345678,'Cardiologist','john.smith@email.com',123456789,'Cardiology'),(2,'Dr. Jane Doe',23456789,'Neurologist','jane.doe@email.com',987654321,'Neurology'),(3,'Dr. Alice Williams',34567890,'General Practitioner','alice.williams@email.com',112233445,'General Medicine'),(4,'Dr. Bob Brown',45678901,'Pediatrician','bob.brown@email.com',223344556,'Pediatrics'),(5,'Dr. Emily Johnson',56789012,'Dermatologist','emily.johnson@email.com',334455667,'Dermatology'),(6,'Dr. Michael White',67890123,'Orthopedic Surgeon','michael.white@email.com',445566778,'Orthopedics'),(7,'Dr. Jessica Green',78901234,'Ophthalmologist','jessica.green@email.com',556677889,'Ophthalmology'),(8,'Dr. David Harris',89012345,'Endocrinologist','david.harris@email.com',667788990,'Endocrinology'),(9,'Dr. Laura Clark',90123456,'Psychiatrist','laura.clark@email.com',778899001,'Psychiatry'),(10,'Dr. Daniel Lewis',10123456,'Gastroenterologist','daniel.lewis@email.com',889900112,'Gastroenterology');
+INSERT INTO `doctor` VALUES (1,'Dr. Juan Perez',12345678,'Cardiologo','juan.perez@email.com',123456789,'Cardiologia'),(2,'Dra. Ana Lopez',23456789,'Neurologo','ana.lopez@email.com',987654321,'Neurologia'),(3,'Dr. Carlos Garcia',34567890,'Medico General','carlos.garcia@email.com',112233445,'Medicina General'),(4,'Dra. Marta Sanchez',45678901,'Pediatra','marta.sanchez@email.com',223344556,'Pediatria'),(5,'Dra. Sofia Rodriguez',56789012,'Dermatologa','sofia.rodriguez@email.com',334455667,'Dermatologia'),(6,'Dr. Miguel Blanco',67890123,'Cirujano Ortopedico','miguel.blanco@email.com',445566778,'Ortopedia'),(7,'Dra. Julia Verde',78901234,'Oftalmologa','julia.verde@email.com',556677889,'Oftalmologia'),(8,'Dr. David Torres',89012345,'Endocrinologo','david.torres@email.com',667788990,'Endocrinologia'),(9,'Dra. Laura Cruz',90123456,'Psiquiatra','laura.cruz@email.com',778899001,'Psiquiatria'),(10,'Dr. Daniel Suarez',10123456,'Gastroenterologo','daniel.suarez@email.com',889900112,'Gastroenterologia');
 /*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +137,7 @@ CREATE TABLE `medicalhistory` (
 
 LOCK TABLES `medicalhistory` WRITE;
 /*!40000 ALTER TABLE `medicalhistory` DISABLE KEYS */;
-INSERT INTO `medicalhistory` VALUES (1,1,1,1,'sf');
+INSERT INTO `medicalhistory` VALUES (1,1,1,1,'1');
 /*!40000 ALTER TABLE `medicalhistory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,7 +193,7 @@ CREATE TABLE `patient` (
 
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient` VALUES (1,'Michael Johnson',35,'michael.johnson@email.com',12345678),(2,'Emily Davis',28,'emily.davis@email.com',23456789),(3,'Liam Martinez',50,'liam.martinez@email.com',34567890),(4,'Sophia Taylor',22,'sophia.taylor@email.com',45678901),(5,'Mia Wilson',45,'mia.wilson@email.com',56789012),(6,'James Moore',60,'james.moore@email.com',67890123),(7,'Olivia Anderson',30,'olivia.anderson@email.com',78901234),(8,'Lucas Thomas',40,'lucas.thomas@email.com',89012345),(9,'Ava Jackson',25,'ava.jackson@email.com',90123456),(10,'Ethan Harris',55,'ethan.harris@email.com',10123456);
+INSERT INTO `patient` VALUES (1,'Miguel Gonzalez',35,'miguel.gonzalez@email.com',12345678),(2,'Lucia Fernandez',28,'lucia.fernandez@email.com',23456789),(3,'Pablo Martinez',50,'pablo.martinez@email.com',34567890),(4,'Sofia Torres',22,'sofia.torres@email.com',45678901),(5,'Mia Ramirez',45,'mia.ramirez@email.com',56789012),(6,'Javier Morales',60,'javier.morales@email.com',67890123),(7,'Olivia Castro',30,'olivia.castro@email.com',78901234),(8,'Lucas Ruiz',40,'lucas.ruiz@email.com',89012345),(9,'Camila Delgado',25,'camila.delgado@email.com',90123456),(10,'Elias Herrera',55,'elias.herrera@email.com',10123456);
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -191,108 +222,6 @@ LOCK TABLES `recipe` WRITE;
 /*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
 /*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Temporary view structure for view `view_consultations`
---
-
-DROP TABLE IF EXISTS `view_consultations`;
-/*!50001 DROP VIEW IF EXISTS `view_consultations`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `view_consultations` AS SELECT 
- 1 AS `consultation_id`,
- 1 AS `patient_name`,
- 1 AS `doctor_name`,
- 1 AS `date`,
- 1 AS `notes`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Temporary view structure for view `view_medical_history`
---
-
-DROP TABLE IF EXISTS `view_medical_history`;
-/*!50001 DROP VIEW IF EXISTS `view_medical_history`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `view_medical_history` AS SELECT 
- 1 AS `history_id`,
- 1 AS `patient_name`,
- 1 AS `doctor_name`,
- 1 AS `details`,
- 1 AS `consultation_date`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Temporary view structure for view `view_recipes`
---
-
-DROP TABLE IF EXISTS `view_recipes`;
-/*!50001 DROP VIEW IF EXISTS `view_recipes`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `view_recipes` AS SELECT 
- 1 AS `recipe_id`,
- 1 AS `patient_name`,
- 1 AS `doctor_name`,
- 1 AS `description`,
- 1 AS `consultation_date`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Final view structure for view `view_consultations`
---
-
-/*!50001 DROP VIEW IF EXISTS `view_consultations`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `view_consultations` AS select `c`.`id` AS `consultation_id`,`p`.`name` AS `patient_name`,`d`.`name` AS `doctor_name`,`c`.`date` AS `date`,`c`.`notes` AS `notes` from ((`consultation` `c` join `patient` `p` on((`c`.`patient_id` = `p`.`id`))) join `doctor` `d` on((`c`.`doctor_id` = `d`.`id`))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `view_medical_history`
---
-
-/*!50001 DROP VIEW IF EXISTS `view_medical_history`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `view_medical_history` AS select `mh`.`id` AS `history_id`,`p`.`name` AS `patient_name`,`d`.`name` AS `doctor_name`,`mh`.`details` AS `details`,`c`.`date` AS `consultation_date` from (((`medicalhistory` `mh` join `patient` `p` on((`mh`.`patient_id` = `p`.`id`))) join `doctor` `d` on((`mh`.`doctor_id` = `d`.`id`))) join `consultation` `c` on((`mh`.`consultation_id` = `c`.`id`))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `view_recipes`
---
-
-/*!50001 DROP VIEW IF EXISTS `view_recipes`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `view_recipes` AS select `r`.`id` AS `recipe_id`,`p`.`name` AS `patient_name`,`d`.`name` AS `doctor_name`,`r`.`description` AS `description`,`c`.`date` AS `consultation_date` from (((`recipe` `r` join `consultation` `c` on((`r`.`consultation_id` = `c`.`id`))) join `patient` `p` on((`c`.`patient_id` = `p`.`id`))) join `doctor` `d` on((`c`.`doctor_id` = `d`.`id`))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -303,4 +232,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-18 13:41:52
+-- Dump completed on 2025-02-21 13:32:28
